81be4513c581b30d894b405126d6e0edd7313e2f
datageekrj <55853415+datageekrj@users.noreply.github.com>
2024-11-29 03:25:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const bottomRoot = document.getElementById("root");
 const allButtons = document.querySelectorAll(".button");
 const aboutSection = document.getElementById("aboutSection");
 const youTubeButton = document.querySelector(".youtube");
-const goBackButton = document.querySelector(".goBack");
+const goBackButton = document.querySelectorAll(".goBack");
 const projectSection = document.getElementById("projectSection");
 const projectButton = document.querySelector(".projects");
 const bodySection = document.querySelector("body");
+const contactSection = document.getElementById("contactSection");
+const contactButton = document.querySelector(".contact");
+const contactMeDirectly = document.getElementById("contactMeDirectly");
+const contactMeDirectlySection = document.getElementById("contactMeDirectlySection");
+const closeContactMeDirectlySection = document.getElementById("closeContactMeDirectlySection");
 
 allButtons.forEach((btn) => {
     btn.addEventListener("click", (event) => {
@@ -16,10 +21,8 @@ allButtons.forEach((btn) => {
     });
 })
 
-
 aboutButton.addEventListener("click", (event) => {
     aboutSection.style.display = "flex";
-    goBackButton.style.display = "block"
 })
 
 youTubeButton.addEventListener("click", (event) => {
@@ -28,19 +31,37 @@ youTubeButton.addEventListener("click", (event) => {
     bottomRoot.style.display = "flex    ";
 })
 
-goBackButton.addEventListener("click", (event) => {
-    topImage.style.display = "block";
-    bottomRoot.style.display = "flex";
-    goBackButton.style.display = "none";
-    aboutSection.style.display = "none";
-    projectSection.style.display = "none";
-    bodySection.style.justifyContent = "center";
-    bodySection.style.alignItems = "";
+goBackButton.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        topImage.style.display = "block";
+        bottomRoot.style.display = "flex";
+        // btn.style.display = "none";
+        aboutSection.style.display = "none";
+        projectSection.style.display = "none";
+        bodySection.style.justifyContent = "center";
+        bodySection.style.alignItems = "";
+        contactSection.style.display = "none";
+    });
 })
 
 projectButton.addEventListener("click", (event) => {
     projectSection.style.display = "flex";
-    goBackButton.style.display = "block";
     bodySection.style.justifyContent = "center";
     bodySection.style.alignItems = "center";
+})
+
+contactButton.addEventListener("click", (event) => {
+    contactSection.style.display = "flex";
+    bodySection.style.justifyContent = "center";
+    bodySection.style.alignItems = "center";
+})
+
+contactMeDirectly.addEventListener("click", (event) => {
+    contactMeDirectlySection.style.display = "flex";
+    // bodySection.style.justifyContent = "center";
+    // bodySection.style.alignItems = "center";
+})
+
+closeContactMeDirectlySection.addEventListener("click", (event) => {
+    contactMeDirectlySection.style.display = "none";
 })
